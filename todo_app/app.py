11260cde94e65387ef8bd402dbed02668135989a
Helen Curtis <helen.curtis@necsws.com>
2022-04-25 15:00:14 +0100

    another update...

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from todo_app.data.session_items import get_items
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_item
 app = Flask(__name__)
 app.config.from_object(Config())
 
