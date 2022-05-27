0694d8127264a1b46f691a8370b6f53e5da2115e
Helen Curtis <helen.curtis@necsws.com>
2022-05-27 14:51:10 +0100

    Module1 submission by Helen Curtis

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request
-from todo_app.data.session_items import get_items
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, get_items
 from todo_app.flask_config import Config
 app = Flask(__name__)
 app.config.from_object(Config())
-
+#Application code for listing predefined todo list from session_items.py
 @app.route('/')
 def index():
     todos = get_items()
     return render_template('index.html',todos=todos)
-
+#Application code to add items when clicking on the button to post to list, uses add_item from session_items.py 
+@app.route('/todo', methods=['POST'])
+def addtodo():
+    title=request.form["title"]
+    add_item(title)
+    return redirect('/')
 
 
 
