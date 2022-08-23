80d9cd2ccc853e894dd83e40b1fdf1585bf9cb9d
Helen Curtis <helen.curtis@necsws.com>
2022-08-23 19:48:53 +0100

    Module 2 Trello key and token now hidden in .env

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -5,6 +5,8 @@
 import requests
 import json
 import os
+from dotenv import load_dotenv
+
 def get_items():
    
   url = "https://api.trello.com/1/boards/62f2b96de0b99c00aff27ae6/lists?"
@@ -12,10 +14,15 @@ def get_items():
   headers = {
     "Accept": "application/json"
     }
- 
+  load_dotenv()
+  trello_key=(os.getenv('TRELLO_KEY'))
+  trello_token=(os.getenv('TRELLO_TOKEN'))
   query = {
-   'key': '9c0d3be68129cbe4d579fe32310123a5',
-    'token': '58a1a2bc161d2f86b519250c5d11683a103222c98d6f53830b29acba9abf0a21',
+
+    'key':trello_key,
+    'token':trello_token,
+   #'key': '9c0d3be68129cbe4d579fe32310123a5',
+   # 'token': '58a1a2bc161d2f86b519250c5d11683a103222c98d6f53830b29acba9abf0a21',
     'cards': 'open',
     'card_fields':'name'
     }
