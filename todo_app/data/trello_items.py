c48acb6db22a045d5e854834ff582df3aa0302b9
Helen Curtis <helen.curtis@necsws.com>
2022-08-23 19:15:12 +0100

    Module 2 Trello items now showing on the list

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -4,6 +4,7 @@
 #from urllib import response
 import requests
 import json
+import os
 def get_items():
    
   url = "https://api.trello.com/1/boards/62f2b96de0b99c00aff27ae6/lists?"
@@ -13,7 +14,7 @@ def get_items():
     }
  
   query = {
-    'key': '9c0d3be68129cbe4d579fe32310123a5',
+   'key': '9c0d3be68129cbe4d579fe32310123a5',
     'token': '58a1a2bc161d2f86b519250c5d11683a103222c98d6f53830b29acba9abf0a21',
     'cards': 'open',
     'card_fields':'name'
@@ -25,8 +26,18 @@ def get_items():
     headers=headers,
     params=query,
     )
-  
-  print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+  #Create a new array
+  apiItems = []
+  #Convert json response to a dictionary item
+  aDict = json.loads(response.text)
+  #Creating a list from a dictionary item and using in the for statement
+  for list in aDict:
+    for card in list['cards']:
+      apiItems.append({"id": card["id"], "title": card["name"], "status": list["name"]})
+  #returnng array of data
+  return apiItems.copy()
+
+  #print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
 get_items()
 
 #def add_item():
