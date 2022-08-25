998cce13ee8249b43351f7cf1b92b5cc2c5475ee
Helen Curtis <helen.curtis@necsws.com>
2022-08-25 20:15:17 +0100

    Created a table to display Trello items

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -44,26 +44,43 @@ def get_items():
 
   #print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
 get_items()
+#New
+def get_item(id):
+    """
+    Fetches the saved item with the specified ID.
 
-#def add_item():
-# url = "https://api.trello.com/1/boards/62f2b96de0b99c00aff27ae6/lists?"
+    Args:
+        id: The ID of the item.
 
-# headers = {
-#   "Accept": "application/json"
-#  }
+    Returns:
+        item: The saved item, or None if no items match the specified ID.
+    """
+    items = get_items()
+    return next((item for item in items if item['id'] == int(id)), None)
+    
+def add_item():
+  url = "https://api.trello.com/1/boards/62f2b96de0b99c00aff27ae6/cards?"
 
-# query = {
-#   'name': '{name}',
-#   'key': 'APIKey',
+  headers = {
+    "Accept": "application/json"
+    }
+  load_dotenv()
+  trello_key=(os.getenv('TRELLO_KEY'))
+  trello_token=(os.getenv('TRELLO_TOKEN'))
+  query = {
+   'key':trello_key,
+   'token':trello_token,
+   #The ID of the list the card should be created in- so To Do
+   'idList': '62f2b96de0b99c00aff27ae6'
 #   'token': 'APIToken'
-#  }
+   }
 
-# response = requests.request(
-#   "POST",
-#   url,
-#   headers=headers,
-#   params=query
-#  )
+  response = requests.request(
+    "POST",
+    url,
+    headers=headers,
+    params=query
+   )
 
 # print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-#add_item()
+add_item()
