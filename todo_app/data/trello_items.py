278847e728d915da557dd2e8a01175af8ae99f6a
Helen Curtis <helen.curtis@necsws.com>
2023-01-19 09:55:56 +0000

    Updated code after comments from module 2

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -5,76 +5,65 @@ from dotenv import load_dotenv
 load_dotenv()
 trello_key=(os.getenv('TRELLO_KEY'))
 trello_token=(os.getenv('TRELLO_TOKEN'))
-#Get Trello items
-def get_items():
-   
-  url = "https://api.trello.com/1/boards/62f2b96de0b99c00aff27ae6/lists?"
+trello_todo_list=(os.getenv('TRELLO_TODO_LIST'))
+trello_done_list=(os.getenv('TRELLO_DONE_LIST'))
+trello_board_url=(os.getenv('TRELLO_BOARD_URL'))
 
+def get_items():   
+  url = trello_board_url
   headers = {
     "Accept": "application/json"
     }
-
   query = {
-
     'key':trello_key,
     'token':trello_token,
     'cards': 'open',
     'card_fields':'name'
     }
- 
   response = requests.request(
     "GET",
     url,
     headers=headers,
     params=query,
     )
-#Create a new array
   apiItems = []
-#Convert json response to a dictionary item
   aDict = json.loads(response.text)
-#Creating a list from a dictionary item and using in the for statement
   for list in aDict:
     for card in list['cards']:
       apiItems.append({"id": card["id"], "title": card["name"], "status": list["name"]})
-#returning array of data
   return apiItems.copy()
 
-#Add new item to Trello To Do List
 def add_item(title):
   url = "https://api.trello.com/1/cards"
 
   headers = {
    "Accept": "application/json"
   }
-
   query = {
-   'idList': '62f2b96de0b99c00aff27aed',
+   'idList': trello_todo_list,
    'key': trello_key,
    'token': trello_token,
    'name': title
   }
-
   response = requests.request(
     "POST",
     url,
     headers=headers,
     params=query
   )
-#find id as variable
+
 def update_item(id):
   url = f"https://api.trello.com/1/cards/{id}" 
 
   headers = {
    "Accept": "application/json"
   }
-#Move to done status, this is the id of done list
   query = {
-   'idList': '62f2b96de0b99c00aff27aef',
+   'idList': trello_done_list,
    'key': trello_key,
    'token': trello_token,
    'id': id
   }
-
   response = requests.request(
     "PUT",
     url,
