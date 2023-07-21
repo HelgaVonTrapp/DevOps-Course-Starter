40f3a5abb67fc31a5fc1cc050b248aba944a6316
Helen Curtis <helen.curtis@necsws.com>
2023-07-21 10:37:46 +0100

    Step 2 login manager added and working

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, redirect,request,render_template
+from flask_login import LoginManager
 from todo_app.data.todo_items import get_items, add_item, update_item
 from todo_app.flask_config import Config
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
+    
+    login_manager = LoginManager()
+    @login_manager.unauthorized_handler
+    def unauthenticated():
+        pass # Add logic to redirect to the GitHub OAuth flow when unauthenticated
+    @login_manager.user_loader
+    def load_user(user_id):
+        pass # We will return to this later
+    login_manager.init_app(app)
 
     @app.route('/')
     def index():
